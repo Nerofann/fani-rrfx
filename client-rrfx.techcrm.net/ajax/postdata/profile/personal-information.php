0129665aac44a3753024ebeb64316ebd9307d0b5
Fani Annur Rozi <crazyhx4@gmail.com>
2025-08-04 23:13:23 +0700

    Create personal-information.php

---
+++ client-rrfx.techcrm.net/ajax/postdata/profile/personal-information.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Models\Helper;
+use Config\Core\Database;
+
+$data = Helper::getSafeInput($_POST);
+$required = [
+    'city' => "Kota",
+    'zip' => "Kodepos",
+    'tempat_lahir' => "Tempat Lahir",
+    'tanggal_lahir' => "Tanggal Lahir",
+    // 'identity_type' => "Tipe Identitas",
+    // 'identity_number' => "Nomor Identitas",
+    'address' => "Alamat Lengkap"
+];
+
+foreach($required as $key => $text) {
+    if(empty($data[ $key ])) {
+        JsonResponse([
+            'success' => false,
+            'message' => "Kolom {$text} wajib diisi",
+            'data' => []
+        ]);
+    }
+}
+
+/* Update **/
+$updateData = [
+    'MBR_CITY' => $data['city'],
+    'MBR_ZIP' => $data['zip'],
+    'MBR_TMPTLAHIR' => $data['tempat_lahir'],
+    'MBR_TGLLAHIR' => date("Y-m-d H:i:s", strtotime($data['tanggal_lahir'])),
+    'MBR_ADDRESS' => $data['address']
+];
+
+$update = Database::update("tb_member", $updateData, ['MBR_ID' => $user['MBR_ID']]);
+if(!$update) {
+    JsonResponse([
+        'success' => false,
+        'message' => "Gagal memperbarui data",
+        'data' => []
+    ]);
+}
+
+JsonResponse([
+    'success' => true,
+    'message' => "Berhasil",
+    'data' => []
+]);
