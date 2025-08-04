43c682de0573f922a400b920b7101d42440c6991
Fani Annur Rozi <crazyhx4@gmail.com>
2025-08-04 23:40:45 +0700

    template email update password

---
+++ config/email/update-password.php
@@ -0,0 +1,105 @@
+<table>
+    <tbody>
+        <tr>
+            <td width="100%" style="font-weight:400;text-align:left;padding-left:32px;padding-right:32px;vertical-align:top;border-top:0;border-right:0;border-bottom:0;border-left:0">
+                <div style="height:32px;line-height:32px;font-size:1px"> &hairsp;</div>
+                <table width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation">
+                    <tbody>
+                        <tr>
+                            <td>
+                            <div align="center" style="font-family:Roboto,Tahoma,Verdana,Segoe,sans-serif;text-align:center">
+                                <table cellpadding="0" cellspacing="0" width="100%" style="border-collapse:collapse;border-spacing:0px">
+                                <tbody>
+                                    <tr>
+                                    <td align="center" valign="middle" style="padding:0px;margin:0px;font-size:0px;width:56px">
+                                        <img alt="" width="56" height="61" style="display:block;border:0;outline:none;text-decoration:none" src="https://ci3.googleusercontent.com/meips/ADKq_NbRF5OAHNNH37D062PDX6ZUsgdn1gr8wbkKDeHI7U038cDk_xxLEkCiRLDKBWuNUdT6o7rKTgEFhcqWjv9bB6mkeBv48THloYgVwAQUR9XqDnlvp2DjM4PI7pCChUxt5sAU2Eo3O8uz0t20psyUocOMXACqOXeDMx5l3sKQDdemXgb5cEvdDKS0pOp1c7COth8ds3E5Qe_yRVCwshA=s0-d-e1-ft#https://kivys.stripocdn.email/content/guids/CABINET_ad45b490c09265f65aa37c74ea0f4b2962d4ca4c6dbd5c0f21862ef41b8994ee/images/letter.png" class="CToWUd" data-bit="iit">
+                                    </td>
+                                    </tr>
+                                </tbody>
+                                </table>
+                            </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div style="height:32px;line-height:32px;font-size:1px"> &hairsp;</div>
+                <table width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation">
+                    <tbody>
+                        <tr>
+                            <td style="text-align:center;width:100%">
+                            <h1 style="margin:0;color:#0b0b16;direction:ltr;font-family:Roboto,Tahoma,Verdana,Segoe,sans-serif;font-size:32px;font-weight:700;letter-spacing:normal;line-height:120%;text-align:center;margin-top:0;margin-bottom:0">
+                                <strong>Email Verification</strong>
+                            </h1>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div style="height:16px;line-height:16px;font-size:1px"> &hairsp;</div>
+                <table width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="word-break:break-word">
+                    <tbody>
+                        <tr>
+                            <td>
+                                <div style="color:#0b0b16;direction:ltr;font-family:Roboto,Tahoma,Verdana,Segoe,sans-serif;font-size:16px;font-weight:400;letter-spacing:0;line-height:150%;text-align:center">
+                                    <p style="margin:0">To continue registration and create your trading account, please confirm your email address.</p>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+    </tbody>
+</table>
+<table align="center" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="background-color:#f3f5fe">
+    <tbody>
+        <tr>
+            <td>
+                <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" width="600" style="background-color:#fff;border-radius:0 0 24px 24px;color:#000;width:600px;margin:0 auto">
+                    <tbody>
+                        <tr>
+                            <td width="100%" style="font-weight:400;text-align:left;padding-left:32px;padding-right:32px;vertical-align:top;border-top:0;border-right:0;border-bottom:0;border-left:0">
+                                <div style="height:24px;line-height:24px;font-size:1px"> &hairsp;</div>
+                                <table width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation">
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                <div align="center" style="font-family:Roboto,Tahoma,Verdana,Segoe,sans-serif;text-align:center">
+                                                    <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="color:#000">
+                                                        <tbody>
+                                                            <tr>
+                                                                <td width="100%" style="font-weight:400;text-align:left;vertical-align:top;border-top:0;border-right:0;border-bottom:0;border-left:0">
+                                                                    <div style="height:5px;line-height:5px;font-size:1px"></div>
+                                                                    <table width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation">
+                                                                        <tbody>
+                                                                            <tr>
+                                                                            <td style="text-align:center">
+                                                                                <div align="center">
+                                                                                    <a href="http://client-tridentpro.test/verification/<?= $code ?>" style="text-decoration:none;display:inline-block;color:#ffffff;background-color:#ACB95D;border-radius:30px;width:auto;border-top:0px solid transparent;font-weight:400;border-right:0px solid transparent;border-bottom:0px solid transparent;border-left:0px solid transparent;padding-top:16px;padding-bottom:16px;font-family:Arial,Helvetica Neue,Helvetica,sans-serif;text-align:center;word-break:keep-all" target="_blank">
+                                                                                        <span style="padding-left:48px;padding-right:48px;font-size:16px;display:inline-block;letter-spacing:normal">
+                                                                                        <span dir="ltr" style="word-break:break-word;line-height:19.2px">Confirm</span>
+                                                                                        </span>
+                                                                                    </a>
+                                                                                </div>
+                                                                            </td>
+                                                                            </tr>
+                                                                        </tbody>
+                                                                    </table>
+                                                                    <div style="height:5px;line-height:5px;font-size:1px"></div>
+                                                                </td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                <div style="height:40px;line-height:40px;font-size:1px"> &hairsp;</div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+    </tbody>
+</table>
