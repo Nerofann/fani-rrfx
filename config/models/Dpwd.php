9b7114455283977c88ab0ee2cc7c8b0c7a515b88
allmediaindo <indooallmedia@gmail.com>
2025-08-06 16:05:02 +0700

    Create model Dpwd

---
+++ config/models/Dpwd.php
@@ -0,0 +1,51 @@
+<?php 
+namespace App\Models;
+
+use Config\Core\Database;
+use Config\Core\SystemInfo;
+use Exception;
+
+class Dpwd {
+
+    public static array $status = [
+        "-1" => [
+            'html' => '<span class="badge bg-success">Open</span>',
+            'text' => "Open"
+        ],
+        "1" => [
+            'html' => '<span class="badge bg-danger">Closed</span>',
+            'text' => "Closed"
+        ]
+    ];
+
+    public static function findByCode(string $code): array|bool {
+        try {
+            $db = Database::connect();
+            $sqlGet = $db->query("SELECT * FROM tb_ticket WHERE TICKET_CODE = '{$code}' LIMIT 1");
+            return $sqlGet->fetch_assoc() ?? false;
+
+        } catch (Exception $e) {
+            if(SystemInfo::isDevelopment()) {
+                throw $e;
+            }
+
+            return false;
+        }
+    }
+
+    public static function findById(string $id): array|bool {
+        try {
+            $db = Database::connect();
+            $sqlGet = $db->query("SELECT * FROM tb_dpwd WHERE MD5(MD5(tb_dpwd.ID_DPWD)) = '{$id}' LIMIT 1");
+            return $sqlGet->fetch_assoc() ?? false;
+
+        } catch (Exception $e) {
+            if(SystemInfo::isDevelopment()) {
+                throw $e;
+            }
+
+            return false;
+        }
+    }
+
+}
