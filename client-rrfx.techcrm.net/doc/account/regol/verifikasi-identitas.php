6b52e0cb38011443357ca35246b19ce11d0a7c3c
Fani Annur Rozi <crazyhx4@gmail.com>
2025-08-06 15:04:12 +0700

    Update verifikasi-identitas.php

--- client-rrfx.techcrm.net/doc/account/regol/verifikasi-identitas.php
+++ client-rrfx.techcrm.net/doc/account/regol/verifikasi-identitas.php
@@ -1,7 +1,7 @@
 <div class="row">
     <div class="col-md-9 mx-auto">
         <form method="post" id="form-verifikasi-identitas">
-            <input type="hidden" name="csrf_token" value="<?= getCSRFToken(); ?>">
+            <input type="hidden" name="csrf_token" value="<?= uniqid(); ?>">
             <div class="card">
                 <div class="card-body">
                     <div class="text-center"><h5>VERIFIKASI IDENTITAS</h5></div>
@@ -12,11 +12,11 @@
                     <div class="row">
                         <div class="col-md-6 mb-3 h-100">
                             <label for="app_foto_terbaru" class="form-label">Foto Terbaru (Selfie) <span class="text-danger">*</span></label>
-                            <input type="file" class="dropify" id="app_foto_terbaru" name="app_foto_terbaru" data-allowed-file-extensions="png jpg jpeg" data-default-file="<?= $aws_folder . $realAccount['ACC_F_APP_FILE_FOTO'] ?>">
+                            <input type="file" class="dropify" id="app_foto_terbaru" name="app_foto_terbaru" data-allowed-file-extensions="png jpg jpeg" data-default-file="<?= App\Models\FileUpload::awsFile($realAccount['ACC_F_APP_FILE_FOTO'] ?? ""); ?>">
                         </div>
                         <div class="col-md-6 mb-3">
                             <label for="app_foto_identitas" class="form-label">Foto KTP <span class="text-danger">*</span></label>
-                            <input type="file" class="dropify" id="app_foto_identitas" name="app_foto_identitas" data-allowed-file-extensions="png jpg jpeg" data-default-file="<?= $aws_folder . $realAccount['ACC_F_APP_FILE_ID'] ?? ""; ?>">
+                            <input type="file" class="dropify" id="app_foto_identitas" name="app_foto_identitas" data-allowed-file-extensions="png jpg jpeg" data-default-file="<?= App\Models\FileUpload::awsFile($realAccount['ACC_F_APP_FILE_ID'] ?? ""); ?>">
                         </div>
                     </div>
 
