69585eaf293431472288cfeed890c9e64e19c973
Fani Annur Rozi <crazyhx4@gmail.com>
2025-08-07 18:57:52 +0700

    create internal transfer

--- client-rrfx.techcrm.net/ajax/postdata/internal-transfer/create.php
+++ client-rrfx.techcrm.net/ajax/postdata/internal-transfer/create.php
@@ -3,6 +3,8 @@
 use App\Factory\MetatraderFactory;
 use App\Models\Account;
 use App\Models\Helper;
+use App\Models\Logger;
+use Config\Core\Database;
 
 $data = Helper::getSafeInput($_POST);
 $required = [
@@ -42,7 +44,7 @@ if(!$toAccount) {
 }
 
 /** check jumlah */
-$jumlah = Helper::stringTonumber($data['jumlah']);
+$jumlah = Helper::stringTonumber($data['amount']);
 if(is_numeric($jumlah) === FALSE || $jumlah <= 0) {
     JsonResponse([
         'success' => false,
@@ -60,30 +62,90 @@ if($fromAccount['RTYPE_RATE'] != $toAccount['RTYPE_RATE']) {
     ]);
 }
 
+/** Check Balance Pengirim */
+$balancePengirim = Account::marginBalance($fromAccount['ACC_LOGIN']);
+if($balancePengirim < $jumlah) {
+    JsonResponse([
+        'success' => false,
+        'message' => "Insufficient balance metatrader",
+        'data' => []
+    ]);
+}
 
-/** Deposit ke akun penerima */
+/** Insert */
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+mysqli_begin_transaction($db);
+
 $code = uniqid();
 $apiManager = MetatraderFactory::apiManager();
-$depositData = [
-    'login' => $fromAccount['ACC_LOGIN'],
+$insert = Database::insert("tb_internal_transfer", [
+    'IT_CODE' => $code,
+    'IT_FROM' => $fromAccount['ACC_LOGIN'],
+    'IT_TO' => $toAccount['ACC_LOGIN'],
+    'IT_AMOUNT' => $jumlah,
+    'IT_AMOUNT_SOURCE' => $jumlah,
+    'IT_CURR_TO' => $toAccount['RTYPE_CURR'],
+    'IT_CURR_FROM' => $fromAccount['RTYPE_CURR'],
+    'IT_RATE_TO' => 1,
+    'IT_RATE_FROM' => 1,
+    // '' => ,
+    // '' => $deposit->ticket,
+    'IT_DATETIME' => date("Y-m-d H:i:s"),
+]);
+
+if(!$insert) {
+    $db->rollback();
+    JsonResponse([
+        'success' => false,
+        'message' => "Invalid Status Save",
+        'data' => []
+    ]);
+}
+
+/** Deposit ke akun penerima */
+$deposit = $apiManager->deposit([
+    'login' => $toAccount['ACC_LOGIN'],
     'amount' => $jumlah,
     'comment' => "IT-{$code}"
-];
+]);
 
-$deposit = $apiManager->deposit($depositData);
-print_r($deposit);
+if(is_object($deposit) === FALSE || !property_exists($deposit, "ticket")) {
+    $db->rollback();
+    JsonResponse([
+        'success' => false,
+        'message' => "Invalid Status Deposit",
+        'data' => []
+    ]);
+}
 
-// /** insert */
-// $insert = Database::insert("tb_internal_transfer", [
-//     'IT_FROM' => $fromAccount,
-//     'IT_TO' => $toAccount,
-//     'IT_AMOUNT' => $jumlah,
-//     'IT_AMOUNT_SOURCE' => $jumlah,
-//     'IT_CURR_TO' => $toAccount['RTYPE_CURR'],
-//     'IT_CURR_FROM' => $fromAccount['RTYPE_CURR'],
-//     'IT_RATE_TO' => 1,
-//     'IT_RATE_FROM' => 1,
-//     'IT_TICKET_FROM' => ,
-//     'IT_TICKET_TO' => ,
-//     'IT_DATETIME' => ,
-// ]);
+/** Withdrawal dari akun pengirim */
+$withdrawal = $apiManager->deposit([
+    'login' => $fromAccount['ACC_LOGIN'],
+    'amount' => ($jumlah * -1),
+    'comment' => "IT-{$code}"
+]);
+
+if(is_object($withdrawal) === FALSE || !property_exists($withdrawal, "ticket")) {
+    $db->rollback();
+    JsonResponse([
+        'success' => false,
+        'message' => "Invalid Status Withdrawal",
+        'data' => []
+    ]);
+}
+
+/** Update Ticket */
+Database::update("tb_internal_transfer", ['IT_TICKET_FROM' => $withdrawal->ticket, 'IT_TICKET_TO' => $deposit->ticket], ['IT_CODE' => $code]);
+Logger::client_log([
+    'mbrid' => $user['MBR_ID'],
+    'module' => "internal-transfer",
+    'message' => "Internal Transfer from " . $data['from-account'] . " to " . $data['to-account'] . " $jumlah USD",
+    'data' => $data 
+]);
+
+$db->commit();
+JsonResponse([
+    'success' => true,
+    'message' => "Berhasil",
+    'data' => []
+]);
