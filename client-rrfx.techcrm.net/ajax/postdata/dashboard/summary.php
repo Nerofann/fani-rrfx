62e85df8ab344200bb6f14abc2c92d4bf0a7a361
Fani Annur Rozi <crazyhx4@gmail.com>
2025-08-08 20:05:08 +0700

    edit: deposit new accounto juga terhitung di summary dashboard

--- client-rrfx.techcrm.net/ajax/postdata/dashboard/summary.php
+++ client-rrfx.techcrm.net/ajax/postdata/dashboard/summary.php
@@ -8,12 +8,12 @@ $sqlGet = $db->query("
         tb_dpwd.DPWD_TYPE,
         tr.ACC_LOGIN,
         trc.RTYPE_CURR,
-        DPWD_CURR_TO
+        DPWD_CURR_FROM
     FROM tb_dpwd
     JOIN tb_racc tr ON (tr.ID_ACC = tb_dpwd.DPWD_RACC)
     JOIN tb_racctype trc ON (trc.ID_RTYPE = tr.ACC_TYPE)
     WHERE MD5(MD5(DPWD_MBR)) = '{$userid}'
-    AND ((tb_dpwd.DPWD_TYPE = 1 AND tb_dpwd.DPWD_STS = -1) OR (tb_dpwd.DPWD_TYPE = 2 AND tb_dpwd.DPWD_STS != 1))
+    AND ((tb_dpwd.DPWD_TYPE IN (1, 3) AND tb_dpwd.DPWD_STS = -1) OR (tb_dpwd.DPWD_TYPE = 2 AND tb_dpwd.DPWD_STS != 1))
     GROUP BY DPWD_TYPE, tr.ACC_LOGIN
 ");
 
@@ -22,10 +22,10 @@ $withdrawal = ['IDR' => 0, 'USD' => 0];
 if($sqlGet) {
     foreach($sqlGet->fetch_all(MYSQLI_ASSOC) as $dpwd) {
         switch(true) {
-            case ($dpwd['DPWD_TYPE'] == 1 && $dpwd['DPWD_CURR_TO'] == "IDR") : $deposit['IDR'] += $dpwd['AMOUNT']; break;
-            case ($dpwd['DPWD_TYPE'] == 1 && $dpwd['DPWD_CURR_TO'] == "USD") : $deposit['USD'] += $dpwd['AMOUNT']; break;
-            case ($dpwd['DPWD_TYPE'] == 2 && $dpwd['DPWD_CURR_TO'] == "IDR") : $withdrawal['IDR'] += $dpwd['AMOUNT']; break;
-            case ($dpwd['DPWD_TYPE'] == 2 && $dpwd['DPWD_CURR_TO'] == "USD") : $withdrawal['USD'] += $dpwd['AMOUNT']; break;
+            case (in_array($dpwd['DPWD_TYPE'], [1, 3]) && $dpwd['DPWD_CURR_FROM'] == "IDR") : $deposit['IDR'] += $dpwd['AMOUNT']; break;
+            case (in_array($dpwd['DPWD_TYPE'], [1, 3]) && $dpwd['DPWD_CURR_FROM'] == "USD") : $deposit['USD'] += $dpwd['AMOUNT']; break;
+            case ($dpwd['DPWD_TYPE'] == 2 && $dpwd['DPWD_CURR_FROM'] == "IDR") : $withdrawal['IDR'] += $dpwd['AMOUNT']; break;
+            case ($dpwd['DPWD_TYPE'] == 2 && $dpwd['DPWD_CURR_FROM'] == "USD") : $withdrawal['USD'] += $dpwd['AMOUNT']; break;
         }
     }
 }
