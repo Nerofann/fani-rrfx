ee5aafd9d6d136e05ff99b1551670feceb60dcd4
allmediaindo <indooallmedia@gmail.com>
2025-08-11 10:00:08 +0700

    allmedia changes

---
+++ config/email/dealer.php
@@ -0,0 +1,10 @@
+<div style="padding: 10px;text-align: justify;">
+    <strong>Informasi Data Demo Account</strong><br>
+    Hi <strong><?= $name ?>, </strong><br>
+    
+    <ul>
+        <li>Login : <?= $login ?></li>
+        <li>Password Master : <?= $metaPassword ?></li>
+        <li>Password Investor : <?= $metaInvestor ?></li>
+    </ul>
+</div>
--- config/models/CompanyProfile.php
+++ config/models/CompanyProfile.php
@@ -1,5 +1,6 @@
 <?php
-namespace App\Models;
+    namespace App\Models;
+    use Config\Core\Database;
 
 class CompanyProfile {
 
@@ -7,5 +8,43 @@ class CompanyProfile {
 
     public static function init() {
         self::$name = $_ENV['APP_NAME'];
+    }
+
+    public static function profilePerusahaan(int $defaultId = 1) {
+        try {
+            $db = Database::connect();
+            $sqlGet = $db->query("SELECT * FROM tb_profile WHERE ID_PROF = $defaultId LIMIT 1");
+            if($sqlGet->num_rows != 1) {
+                return [];
+            }
+
+            return $sqlGet->fetch_assoc();
+
+        } catch (Exception $e) {
+            if(ini_get("dsplay_errors") == "1") {
+                throw $e;
+            }
+
+            return [];
+        }
+    } 
+    
+    public static function getMainOffice(int $defaultId = 1) {
+        try {
+            $db = Database::connect();
+            $sqlGet = $db->query("SELECT * FROM tb_office WHERE ID_OFC = $defaultId LIMIT 1");
+            if($sqlGet->num_rows != 1) {
+                return [];
+            }
+
+            return $sqlGet->fetch_assoc();
+
+        } catch (Exception $e) {
+            if(ini_get("dsplay_errors") == "1") {
+                throw $e;
+            }
+
+            return [];
+        }
     }
 }
