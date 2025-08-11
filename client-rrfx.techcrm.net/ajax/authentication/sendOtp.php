90d43a3e6da88de86490af62e5cc44cb0f7ed3d2
Fani Annur Rozi <crazyhx4@gmail.com>
2025-08-11 10:56:09 +0700

    edit: menambahkan metode pengiriman otp menggunakan whatsapp dengan api dari wapanel

--- client-rrfx.techcrm.net/ajax/authentication/sendOtp.php
+++ client-rrfx.techcrm.net/ajax/authentication/sendOtp.php
@@ -1,6 +1,10 @@
 <?php
+
+use App\Factory\ApiWapanelFactory;
 use App\Models\Helper;
 use App\Factory\VerihubFactory;
+use Config\Core\ApiWapanel;
+use Config\Core\Database;
 
 $verihub = VerihubFactory::init();
 $data = Helper::getSafeInput($_POST);
@@ -17,11 +21,39 @@ foreach($required as $req => $text) {
             'data' => []
         ]);
     }
-} 
+}
+
+/** Check Phone Number */
+$phone = $verihub->phoneValidation($data['phone_code'], $data['phone']);
+if(!$phone){
+    JsonResponse([
+        'success' => false,
+        'message' => "Invalid Phone Number",
+        'data' => []
+    ]);
+}
+
+/** Check Terakhir Mengirim */
+$sqlCheckOtp = $db->query("SELECT * FROM tb_member_pending WHERE MBR_PENDING_PHONE = {$phone} ORDER BY ID_MBR_PENDING DESC LIMIT 1");
+if($sqlCheckOtp->num_rows == 1) {
+    $memberPending = $sqlCheckOtp->fetch_assoc();
+    $timenow = time();
+    $timeExpired = strtotime($memberPending['MBR_PENDING_OTP_EXPIRED']);
+    if($timeExpired > $timenow) {
+        JsonResponse([
+            'success' => false,
+            'message' => "You have to wait a few minutes to send again",
+            'data' => []
+        ]);
+    }
+}
 
 // $type = Helper::form_input($_POST['type'] ?? "");
 // $type = strtolower($type);
-$type = "sms";
+$type = "whatsapp";
+$otp = random_int(1000, 9999);
+$expiredSeconds = 0;
+$sendOtp = false;
 if(empty($type) || !in_array($type, ['whatsapp', 'sms'])) {
     JsonResponse([
         'success' => false,
@@ -30,15 +62,28 @@ if(empty($type) || !in_array($type, ['whatsapp', 'sms'])) {
     ]);
 }
 
-$otp = random_int(1000, 9999);
-$sendOtp = false;
 switch($type) {
     case "whatsapp":
-        // $sendOtp = Zenziva::sendOtp_WaReguler($user['MBR_PHONE'], $otp);
-        $sendOtp = false;
+        $expiredSeconds = 300; // 5 Menit
+        $apiWapanel = ApiWapanelFactory::init();
+        $sendOtp = $apiWapanel->sendMessage(['phone' => $phone, 'message' => "Kode OTP {$otp}"]);
+        if(!is_array($sendOtp) || !array_key_exists("status", $sendOtp)) {
+            JsonResponse([
+                'success' => false,
+                'message' => $sendOtp['message'] ?? "Invalid Message Status",
+                'data' => []
+            ]);
+        }
+
+        $sendOtp = [
+            'success' => $sendOtp['status'],
+            'message' => $sendOtp['message'] ?? "",
+            'data' => $sendOtp['data']
+        ];
         break;
 
     case "sms":
+        $expiredSeconds = 90; // 1 menit 30 detik
         $phone = $verihub->phoneValidation($data['phone_code'], $data['phone']);
         if(!$phone){
             JsonResponse([
@@ -47,29 +92,13 @@ switch($type) {
                 'data' => []
             ]);
         }
-
-        /** Check Terakhir Mengirim */
-        $sqlCheckOtp = $db->query("SELECT LOGVER_DATA, LOGVER_DATETIME FROM tb_log_verihub WHERE LOGVER_MBR = {$phone} AND LOGVER_MODULE = '/v1/otp/send' ORDER BY ID_LOGVER DESC LIMIT 1");
-        if($sqlCheckOtp->num_rows == 1) {
-            $timelimit = 90;
-            $timenow = time();
-            $logver = $sqlCheckOtp->fetch_assoc();
-            $logverdata = json_decode($logver['LOGVER_DATA'], true);
-            if($logverdata['time_limit']) {
-                $timelimit = $logverdata['time_limit'];
-            }
-
-            $delay = strtotime("+{$timelimit} second", strtotime($logver['LOGVER_DATETIME']));
-            if($delay > $timenow) {
-                JsonResponse([
-                    'success' => false,
-                    'message' => "You have to wait a few minutes to send again",
-                    'data' => []
-                ]);
-            }
-        }
-
-        $sendOtp = $verihub->sendOtp_sms(['phone' => $phone, 'otp' => $otp]);
+        
+        $sendVerihub = $verihub->sendOtp_sms(['phone' => $phone, 'otp' => $otp]);
+        $sendOtp = [
+            'success' => $sendVerihub['success'],
+            'message' => $sendVerihub['message'] ?? "",
+            'data' => $sendVerihub['data']
+        ];
         break;
 }
 
@@ -89,10 +118,17 @@ if(!$sendOtp['success']) {
     ]);
 }
 
+/** Insert member pending */
+$datetime = date("Y-m-d H:i:s");
+$dateExpired = date("Y-m-d H:i:s", strtotime("+ {$expiredSeconds} second"));
+$sqlInsert = $db->prepare("INSERT INTO tb_member_pending (MBR_PENDING_PHONE, MBR_PENDING_OTP, MBR_PENDING_OTP_SEND, MBR_PENDING_OTP_EXPIRED, MBR_PENDING_OTP_METHOD) VALUES (?,?,?,?,?) ON DUPLICATE KEY UPDATE MBR_PENDING_OTP = ?, MBR_PENDING_OTP_SEND = ?, MBR_PENDING_OTP_EXPIRED = ?, MBR_PENDING_OTP_METHOD = ?");
+$sqlInsert->bind_param("sssssssss", $phone, $otp, $datetime, $dateExpired, $type, $otp, $datetime, $dateExpired, $type);
+$sqlInsert->execute();
+
 JsonResponse([
     'success' => true,
     'message' => "OTP code was successfully sent",
     'data' => [
-        'delay' => 90
+        'delay' => $expiredSeconds
     ]
 ]);
