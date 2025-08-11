2419796ec61c255f3de5b0414353d39ee18cb6b4
Fani Annur Rozi <crazyhx4@gmail.com>
2025-08-11 09:56:07 +0700

    edit: web-trade, menambahkan tampilan saat belum mempunyai real account

--- client-rrfx.techcrm.net/doc/web-trade/index.php
+++ client-rrfx.techcrm.net/doc/web-trade/index.php
@@ -1,1 +1,15 @@
-<?php require_once __DIR__ . "/custom-web-trade/index.php"; ?>
+<?php if(count(App\Models\Account::myAccount($user['MBR_ID'])) > 0) : ?>
+    
+    <?php require_once __DIR__ . "/custom-web-trade/index.php"; ?>
+
+<?php else : ?>
+    <div class="panel">
+        <div class="card">
+            <div class="card-body">
+                <div class="text-center">
+                    <a href="/create-acc" class="text-center btn btn-md btn-primary mt-3 mb-3">Create Real Account</a>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php endif; ?>
