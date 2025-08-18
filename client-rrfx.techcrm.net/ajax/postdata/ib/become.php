d553eb90513d6646386abeefbd69bb6f9551e5c2
Fani Annur Rozi <crazyhx4@gmail.com>
2025-08-18 10:08:44 +0700

    ajax post become ib

--- client-rrfx.techcrm.net/ajax/postdata/ib/become.php
+++ client-rrfx.techcrm.net/ajax/postdata/ib/become.php
@@ -1,22 +1,58 @@
 <?php
 
 use App\Models\Helper;
+use App\Models\Ib;
+use App\Models\User;
+use Config\Core\Database;
 
+$isAllowToBecomeIb = Ib::isAllowToBecomeIb(md5(md5($user['MBR_ID'])));
 $data = Helper::getSafeInput($_POST);
 
 /** check terms & condition */
 if(empty($data['terms'])) {
     JsonResponse([
         'success' => false,
-        'message' => "Please agree the Terms & Policy",
+        'message' => "Mohon setuju dengan Ketentuan & Kebijakan",
         'data' => []
     ]);
 }
 
-if(validate_become_ib($user['MBR_ID']) !== true) {
+if($isAllowToBecomeIb['success'] !== true) {
     JsonResponse([
         'success' => false,
-        'message' => "Has not met the requirements to submit IB",
+        'message' => "Belum memenuhi persyaratan untuk mengajukan IB",
         'data' => []
     ]);
 }
+
+/** Check apakah sudah pernah request berhasil / pending */
+$ibData = User::get_ib_data($user['MBR_ID'], [0, -1]);
+if(is_array($ibData)) {
+    JsonResponse([
+        'success' => false,
+        'message' => "Pengajuan telah dibuat",
+        'data' => []
+    ]);
+}
+
+/** Insert Become ib */
+$jsonData = json_encode([...$data, $isAllowToBecomeIb]);
+$insert = Database::insert("tb_become_ib", [
+    'BECOME_MBR' => $user['MBR_ID'],
+    'BECOME_DATA' => $jsonData,
+    'BECOME_DATETIME' => date("Y-m-d H:i:s")
+]);
+
+if(!$insert) {
+    JsonResponse([
+        'success' => false,
+        'message' => "Pengajuan telah dibuat",
+        'data' => []
+    ]);
+}
+
+JsonResponse([
+    'success' => true,
+    'message' => "Successfull",
+    'data' => []
+]);
