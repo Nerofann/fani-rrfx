0f3678a525d17944e224f7096f73a065cef8b8e5
Fani Annur Rozi <crazyhx4@gmail.com>
2025-08-18 10:08:11 +0700

    Update Account.php

--- config/models/Account.php
+++ config/models/Account.php
@@ -250,41 +250,6 @@ class Account {
         }
     }
 
-    public static function haveStandartAccount(string $userid) {
-        try {
-            global $db;
-            $sqlGet = $db->query("
-                SELECT 
-                    ID_ACC, 
-                    ACC_LOGIN, 
-                    RTYPE_CURR, 
-                    ACC_TYPE, 
-                    RTYPE_TYPE,
-                    ACC_PASS,
-                    ACC_INVESTOR,
-                    mt5u.BALANCE,
-                    mt5u.MARGIN_FREE
-                FROM tb_racc 
-                JOIN tb_racctype ON (ID_RTYPE = ACC_TYPE)
-                JOIN mt5_users mt5u ON (mt5u.LOGIN = ACC_LOGIN)
-                WHERE MD5(MD5(ACC_MBR)) = '{$userid}'
-                AND ACC_DERE = 1 
-                AND ACC_LOGIN != 0
-                AND ACC_WPCHECK = 6
-                AND UPPER(RTYPE_TYPE) IN ('STANDARD', 'STANDARD-PLUS')
-            ");
-
-            if($sqlGet->num_rows == 0) {
-                return [];
-            }
-
-            return $sqlGet->fetch_all(MYSQLI_ASSOC);
-
-        } catch (Exception $e) {
-            return [];
-        }
-    }
-
     public static function getDemoAccount(string $userid): array {
         try {
             global $db;
