4c8041099eefaedd0c850942a9a05f656a118a14
Fani Annur Rozi <crazyhx4@gmail.com>
2025-08-23 22:57:49 +0700

    Update disclosure-statement.php

--- admin-ragondav.techcrm.net/documents/disclosure-statement.php
+++ admin-ragondav.techcrm.net/documents/disclosure-statement.php
@@ -1,46 +1,21 @@
 <?php
+use App\Models\Account;
+use App\Models\Dpwd;
+use App\Models\Helper;
+use App\Models\FileUpload;
+use App\Models\CompanyProfile;
+$realAccount     = Account::realAccountDetail(($acc ?? ""));
+$accnd           = Account::accoundCondition($realAccount['ID_ACC']);
+$depositData     = Dpwd::findByRaccId($realAccount["ID_ACC"]);
+$COMPANY_PRF     = CompanyProfile::profilePerusahaan();
+$COMPANY_MOF     = CompanyProfile::getMainOffice();
+$company         = CompanyProfile::$name;
+$bank            = explode("/", $depositData['DPWD_BANKSRC']);
+$bankName        = $bank[0] ?? "-";
+$bankAccount     = $bank[1] ?? "-";
+$bankHolder      = $bank[2] ?? "-";
 
-    use App\Models\Account;
-    use App\Models\Dpwd;
-    use App\Models\Helper;
-    use App\Models\FileUpload;
-    use App\Models\CompanyProfile;
-
-    $realAccount     = Account::realAccountDetail(($acc ?? ""));
-    $accnd           = Account::accoundCondition($realAccount['ID_ACC']);
-    $depositData     = Dpwd::findByRaccId($realAccount["ID_ACC"]);
-    $COMPANY_PRF     = CompanyProfile::profilePerusahaan();
-    $COMPANY_MOF     = CompanyProfile::getMainOffice();
-    $company         = CompanyProfile::$name;
-    $bank            = explode("/", $depositData['DPWD_BANKSRC']);
-    $bankName        = $bank[0] ?? "-";
-    $bankAccount     = $bank[1] ?? "-";
-    $bankHolder      = $bank[2] ?? "-";
-
-    $bapakatauibu = (!empty($realAccount['ACC_F_APP_PRIBADI_KELAMIN']) && $realAccount['ACC_F_APP_PRIBADI_KELAMIN'] == "Laki-laki")
-    ? 'Bapak/<strike>Ibu</strike>'
-    : '<strike>Bapak</strike>/Ibu';
-?>
-<?php
-
-    use App\Models\Account;
-    use App\Models\Dpwd;
-    use App\Models\Helper;
-    use App\Models\FileUpload;
-    use App\Models\CompanyProfile;
-
-    $realAccount     = Account::realAccountDetail(($acc ?? ""));
-    $accnd           = Account::accoundCondition($realAccount['ID_ACC']);
-    $depositData     = Dpwd::findByRaccId($realAccount["ID_ACC"]);
-    $COMPANY_PRF     = CompanyProfile::profilePerusahaan();
-    $COMPANY_MOF     = CompanyProfile::getMainOffice();
-    $company         = CompanyProfile::$name;
-    $bank            = explode("/", $depositData['DPWD_BANKSRC']);
-    $bankName        = $bank[0] ?? "-";
-    $bankAccount     = $bank[1] ?? "-";
-    $bankHolder      = $bank[2] ?? "-";
-
-    $bapakatauibu = (!empty($realAccount['ACC_F_APP_PRIBADI_KELAMIN']) && $realAccount['ACC_F_APP_PRIBADI_KELAMIN'] == "Laki-laki")
+$bapakatauibu = (!empty($realAccount['ACC_F_APP_PRIBADI_KELAMIN']) && $realAccount['ACC_F_APP_PRIBADI_KELAMIN'] == "Laki-laki")
     ? 'Bapak/<strike>Ibu</strike>'
     : '<strike>Bapak</strike>/Ibu';
 ?>
