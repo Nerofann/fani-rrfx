5d1b96d22e13cc550905c51099eba96459125f70
Fani Annur Rozi <crazyhx4@gmail.com>
2025-08-24 04:03:00 +0700

    edit: root path pada halaman admin tidak bisa diklik

--- admin-ragondav.techcrm.net/doc/admin/update.php
+++ admin-ragondav.techcrm.net/doc/admin/update.php
@@ -18,9 +18,8 @@ if(!$admin) {
     <div>
         <h2 class="main-content-title tx-24 mg-b-5">Update Admin</h2>
         <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="javascript:void(0);">Home</a></li>
-            <li class="breadcrumb-item"><a href="javascript:void(0);">Admin</a></li>
-            <li class="breadcrumb-item"><a href="javascript:void(0);">List</a></li>
+            <li class="breadcrumb-item"><a href="/dashboard">Home</a></li>
+            <li class="breadcrumb-item"><a href="/admin/view">Admin</a></li>
             <li class="breadcrumb-item active" aria-current="page"><a href="javascript:void(0);">Update</a></li>
         </ol>
     </div>
