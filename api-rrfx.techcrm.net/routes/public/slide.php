1b8120deb979bdc88b5ecd7a6d30510289562d77
Fani Annur Rozi <crazyhx4@gmail.com>
2025-09-04 19:24:45 +0700

    fix: public/slide

--- api-rrfx.techcrm.net/routes/public/slide.php
+++ api-rrfx.techcrm.net/routes/public/slide.php
@@ -1,24 +1,27 @@
 <?php
+use App\Models\FileUpload;
 
-    $data = [];
-    $result = $db->query("SELECT * FROM tb_slide WHERE SLD_TYPE = 1");
-    if($result->num_rows < 1){
-        ApiResponse([
-            'status' => false,
-            'message' => 'Data slide belum tersedia',
-            'response' => []
-        ], 400);
-    }else{
-        foreach($result->fetch_all(MYSQLI_ASSOC) as $row){
-            $data[] = array(
-                "id" => md5(md5($row['ID_SLD'])),
-                "picture" => $aws_folder.$row['SLD_IMG'],
-                "link" => $row['SLD_LINK']
-            );
-        }
-        ApiResponse([
-            'status' => true,
-            'message' => 'Data slide tersedia',
-            'response' => $data
-        ], 200);
+$data = [];
+$result = $db->query("SELECT * FROM tb_slide WHERE SLD_TYPE = 1");
+if($result->num_rows < 1){
+    ApiResponse([
+        'status' => false,
+        'message' => 'Data slide belum tersedia',
+        'response' => []
+    ], 400);
+
+}else{
+    foreach($result->fetch_all(MYSQLI_ASSOC) as $row){
+        $data[] = array(
+            "id" => md5(md5($row['ID_SLD'])),
+            "picture" => FileUpload::awsFile($row['SLD_IMG']),
+            "link" => $row['SLD_LINK']
+        );
     }
+    
+    ApiResponse([
+        'status' => true,
+        'message' => 'Data slide tersedia',
+        'response' => $data
+    ], 200);
+}
