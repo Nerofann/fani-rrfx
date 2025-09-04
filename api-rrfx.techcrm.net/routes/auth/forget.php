cf29dc572fb38666603d1427f87cfabc6a5c941b
Fani Annur Rozi <crazyhx4@gmail.com>
2025-09-04 14:37:04 +0700

    fix: auth forgot password, mengirimkan link url untuk reset kode

--- api-rrfx.techcrm.net/routes/auth/forget.php
+++ api-rrfx.techcrm.net/routes/auth/forget.php
@@ -1,52 +1,58 @@
 <?php
 
+use App\Models\Helper;
 use App\Models\User;
+use Config\Core\EmailSender;
 
-$email = form_input($_POST['email']);
+$email = Helper::form_input($_POST['email'] ?? "");
 if(empty($email)) {
     ApiResponse([
         'status' => false,
-        'message' => 'Email is required',
+        'message' => 'Kolom email diperlukan',
         'response' => []
     ], 400);
 }
 
-$user = User::findByEmail($email);
-if(empty($user)) {
+/** Check Email */
+$sqlGet = $db->query("SELECT MBR_ID, MBR_EMAIL, MBR_NAME, MBR_RESET_CODE, MBR_RESET_EXPIRED FROM tb_member WHERE LOWER(MBR_EMAIL) = LOWER('{$email}') LIMIT 1");
+if($sqlGet->num_rows != 1) {
     ApiResponse([
         'status' => false,
-        'message' => 'Email not registered',
+        'message' => "Email tidak terdaftar",
         'response' => []
-    ], 400);
+    ]);
 }
 
-$new_pass = strtolower(uniqid());
-$passwordHash   = password_hash($new_pass, PASSWORD_BCRYPT);
-
-/** Update Password */
-$sqlUpdate = $db->prepare("UPDATE tb_member SET MBR_PASS = ? WHERE ID_MBR = ?");
-$sqlUpdate->bind_param("si", $passwordHash, $user['ID_MBR']);
-if(!$sqlUpdate->execute()) {
+$userData = $sqlGet->fetch_assoc();
+if(time() <= strtotime($userData['MBR_RESET_EXPIRED'] ?? time())) {
     ApiResponse([
-        'status'    => false,
-        'message'   => "Failed to update password",
-        'response'  => []
-    ], 400);
+        'status' => false,
+        'message' => "Anda dapat mengirim lagi jika kode sebelumnya telah kedaluwarsa",
+        'response' => []
+    ]);
+}
+
+$resetCode = md5(md5(uniqid() . $userData['MBR_ID']));
+$setCode = User::setResetPasswordCode($userData['MBR_ID'], $resetCode);
+if(!$setCode) {
+    ApiResponse([
+        'status' => false,
+        'message' => "Gagal",
+        'response' => []
+    ]);
 }
 
 /** Send Mail */
 $emailData = [
-    "name"          => $user["MBR_NAME"],
-    "email"         => $user["MBR_EMAIL"],
-    "newPassword"   => $new_pass,
-    "subject"       => "Forget Password $web_name_full " .date("Y-m-d H:i:s"),
-    "comp"          => $web_name_full
+    'subject' => "Reset Password",
+    'code' => $resetCode
 ];
 
-$sendEmail = new SendEmail();
-$sendEmail->useDefault()->useFile("forget", $emailData)->destination($user["MBR_EMAIL"], $user["MBR_NAME"])->send();
+$emailSender = EmailSender::init(['email' => $userData['MBR_EMAIL'], 'name' => $userData['MBR_NAME']]);
+$emailSender->useFile("reset-password", $emailData)->send();
+
 ApiResponse([
     'status' => true,
-    'message' => 'Successfully sent new password to email '.$email,
+    'message' => 'Link reset password telah dikirimkan ke email '.$email,
     'response' => []
-], 200);
+]);
