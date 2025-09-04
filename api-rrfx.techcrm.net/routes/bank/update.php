6f263ac04fe18e8712ca8655b8a0e9e82128d1dd
Fani Annur Rozi <crazyhx4@gmail.com>
2025-09-04 19:13:37 +0700

    fix: api update bank

--- api-rrfx.techcrm.net/routes/bank/update.php
+++ api-rrfx.techcrm.net/routes/bank/update.php
@@ -1,119 +1,94 @@
 <?php
 
-use App\Models\Database;
+use App\Models\BankList;
+use App\Models\Helper;
+use App\Models\User;
+use Config\Core\Database;
 
-$data = $helperClass->getSafeInput($_POST);
+$data = Helper::getSafeInput($_POST);
 $required = [
-    'bank_id' => "ID bank",
-    'currency' => "Bank Currency",
-    'bank_name' => "Nama Bank",
-    'bank_holder' => "Nama pemilik bank",
-    'account' => "No. Rekening",
-    'type' => "Tipe"
+    'id' => "ID bank",
+    'name' => "Nama pemilik rekening",
+    'bank-name' => "Nama Bank",
+    'bank-number' => "Nomor Rekening"
 ];
 
-/** Check Id Bank */
-$sqlCheckIdBank = $db->query("SELECT * FROM tb_member_bank WHERE MD5(MD5(ID_MBANK)) = '".$data['bank_id']."' LIMIT 1");
-$bankDetail = $sqlCheckIdBank->fetch_assoc();
-if($sqlCheckIdBank->num_rows != 1) {
-    ApiResponse([
-        'status' => false,
-        'message' => "ID Bank tidak valid",
-        'response' => []
-    ], 400);
-}
-
-foreach($required as $key => $req) {
-    if(empty($data[ $key ])) {
+foreach($required as $req => $text) {
+    if(empty($data[ $req ])) {
         ApiResponse([
             'status' => false,
-            'message' => "{$req} wajib diisi",
+            'message' => "Kolom {$text} harus diisi",
             'response' => []
-        ], 400);
+        ]);
     }
 }
 
-/** check tipe */
-$bankType = strtoupper($data['type']);
-if(!in_array($bankType, ['TABUNGAN', 'GIRO', 'LAINNYA'])) {
+/** check id */
+$bank = User::myBank($user['MBR_ID'], $data['id']);
+if(!$bank) {
     ApiResponse([
         'status' => false,
-        'message' => "Tipe bank tidak valid",
+        'message' => "Invalid ID",
         'response' => []
-    ], 400);
+    ]);
 }
 
-/** check nama bank */
-$bankName = $data['bank_name'];
-$sqlCheckBankName = $db->query("SELECT * FROM tb_banklist WHERE BANKLST_NAME = '{$bankName}' LIMIT 1");
-if($sqlCheckBankName->num_rows != 1) {
+/** check Nomor rekening */
+$rekening = Helper::stringTonumber($data['bank-number']);
+if(is_numeric($rekening) === FALSE || $rekening <= 0) {
+    ApiResponse([
+        'status' => false,
+        'message' => "Nomor rekening tidak valid",
+        'response' => []
+    ]);
+}
+
+$sqlCheck = $db->query("SELECT ID_MBANK FROM tb_member_bank WHERE MBANK_MBR = ".$user['MBR_ID']." AND MBANK_ACCOUNT = {$rekening} AND ID_MBANK != ".$bank['ID_MBANK']." LIMIT 1");
+if($sqlCheck->num_rows != 0) {
+    ApiResponse([
+        'status' => false,
+        'message' => "Nomor Rekening sudah terdaftar",
+        'response' => []
+    ]);
+}
+
+/** Check Nama Bank */
+$bankName = BankList::findByName($data['bank-name']);
+if(!$bankName) {
     ApiResponse([
         'status' => false,
         'message' => "Nama Bank tidak valid",
         'response' => []
-    ], 400);
+    ]);
 }
 
-/** Check currency */
-if(!in_array(strtoupper($data['currency']), ['IDR', 'USD'])) {
+/** validasi bank name */
+if(!preg_match('/^[a-zA-Z\s]+$/', $data['name'])) {
     ApiResponse([
         'status' => false,
-        'message' => "Bank Currency tidak valid",
+        'message' => "Nama Pemilik Rekening tidak valid",
         'response' => []
-    ], 400);
+    ]);
 }
 
-/** Check nomor rekening */
-$userBanks = myBank($userData['MBR_ID']);
-if(is_numeric($data['account']) === FALSE || $data['account'] <= 0) {
-    ApiResponse([
-        'status' => false,
-        'message' => "Nomor rekening harus berupa angka",
-        'response' => []
-    ], 400);
-}
-
-foreach($userBanks as $bank) {
-    if($bank['MBANK_ACCOUNT'] == $data['account'] && $bank['ID_MBANK'] != $bankDetail['ID_MBANK']) {
-        ApiResponse([
-            'status' => false,
-            'message' => "Nomor rekening sudah didaftarkan",
-            'response' => []
-        ], 400);
-    }
-}
-
-/** update */
-$bankBranch = $data['bank_branch'] ?? NULL;
+/** Update */
 $updateData = [
-    'MBANK_HOLDER' => $data['bank_holder'],
-    'MBANK_NAME' => $bankName,
-    'MBANK_ACCOUNT' => $data['account'],
-    'MBANK_BRANCH' => $bankBranch,
-    'MBANK_CURR' => strtoupper($data['currency']),
-    'MBANK_TYPE' => $bankType,
+    'MBANK_HOLDER' => $data['name'],
+    'MBANK_NAME' => $data['bank-name'],
+    'MBANK_ACCOUNT' => $rekening,
 ];
 
-$update = Database::updateWithArray("tb_member_bank", $updateData, ['ID_MBANK' => $bankDetail['ID_MBANK']]);
+$update = Database::update("tb_member_bank", $updateData, ['ID_MBANK' => $bank['ID_MBANK']]);
 if(!$update) {
     ApiResponse([
         'status' => false,
         'message' => "Gagal memperbarui bank",
         'response' => []
-    ], 400);
+    ]);
 }
 
-newInsertLog([
-    'mbrid' => $userData['MBR_ID'],
-    'module' => "bank",
-    'ref' => $data['account'],
-    'message' => "Memperbarui bank, id: ".$data['bank_id'],
-    'device' => "mobile",
-    'data'  => json_encode($data)
-]);
-    
 ApiResponse([
     'status' => true,
-    'message' => "Berhasil memperbarui bank",
+    'message' => "Bank berhasil diperbarui",
     'response' => []
 ]);
