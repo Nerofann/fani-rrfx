0be07ed75261c6604bafaacefb6725de26bd6c97
Fani Annur Rozi <crazyhx4@gmail.com>
2025-09-04 19:42:57 +0700

    fix: api list ticket

--- api-rrfx.techcrm.net/routes/ticket/list.php
+++ api-rrfx.techcrm.net/routes/ticket/list.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Ticket;
+
 $sqlGet = $db->query("SELECT * FROM tb_ticket WHERE MD5(MD5(TICKET_MBR)) = '{$userId}' ORDER BY TICKET_DATETIME DESC");
 $result = [];
 if($sqlGet) {
@@ -7,7 +9,7 @@ if($sqlGet) {
         $result[] = [
             'code' => $ticket['TICKET_CODE'],
             'subject' => $ticket['TICKET_SUBJECT'],
-            'status' => $ticket['TICKET_STS'],
+            'status' => strtolower(Ticket::$status[ $ticket['TICKET_STS'] ]['text'] ?? ""),
             'created_at' => date("Y-m-d H:i:s", strtotime($ticket['TICKET_DATETIME'])),
             'closed_at' => (empty($ticket['TICKET_DATETIME_CLOSE']))? "" : date("Y-m-d H:i:s", strtotime($ticket['TICKET_DATETIME_CLOSE'])),
         ];
