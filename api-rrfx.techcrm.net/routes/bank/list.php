18814c8906d9734835c0725b90e4fc57f88397eb
Fani Annur Rozi <crazyhx4@gmail.com>
2025-09-04 19:13:23 +0700

    fix: api list my bank

--- api-rrfx.techcrm.net/routes/bank/list.php
+++ api-rrfx.techcrm.net/routes/bank/list.php
@@ -1,16 +1,15 @@
 <?php
 
-$userBanks = myBank($userData['MBR_ID']);
+use App\Models\User;
+
+$userBanks = User::myBank($user['MBR_ID']);
 $result = [];
 foreach($userBanks as $bank) {
     $result[] = [
         'id' => md5(md5($bank['ID_MBANK'])),
         'holder' => $bank['MBANK_HOLDER'],
         'name' => $bank['MBANK_NAME'],
-        'currency' => $bank['MBANK_CURR'],
         'account' => $bank['MBANK_ACCOUNT'],
-        'branch' => $bank['MBANK_BRANCH'],
-        'type' => $bank['MBANK_TYPE']
     ];
 }
 
