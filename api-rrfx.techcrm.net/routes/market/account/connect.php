0e79ce33881c1924a37931ddc806010353bc6493
Fani Annur Rozi <crazyhx4@gmail.com>
2025-09-10 12:24:13 +0700

    Update connect.php

--- api-rrfx.techcrm.net/routes/market/account/connect.php
+++ api-rrfx.techcrm.net/routes/market/account/connect.php
@@ -18,53 +18,14 @@ if(empty($account) || $account['ACC_MBR'] != $user['MBR_ID']) {
     ], 400);
 }
 
-$isEmptyToken = empty($account['ACC_TOKEN']);
-$token = "";
-switch($isEmptyToken) {
-    case true:
-        /** Connect meta */
-        $connectData = [
-            'login' => $account['ACC_LOGIN'], 
-            'password' => $account['ACC_PASS']
-        ];
-        
-        $token = $apiTerminal->connect($connectData);
-        if(!$token) {
-            ApiResponse([
-                'status' => false,
-                'message' => "Invalid Connection",
-                'response' => []
-            ], 404);
-        }
-
-        Database::update("tb_racc", ['ACC_TOKEN' => $token], ['ID_ACC' => $account['ID_ACC']]);
-        break;
-
-    case false:
-        /** check connection with available token */
-        $account = $apiTerminal->accountSummary(['id' => $account['ACC_TOKEN']]);
-        if(!$account->success) {
-            /** get new token */
-            $connectData = [
-                'login' => $account['ACC_LOGIN'], 
-                'password' => $account['ACC_PASS']
-            ];
-            
-            $token = $apiTerminal->connect($connectData);
-            if(!$token) {
-                ApiResponse([
-                    'status' => false,
-                    'message' => "Invalid Connection (2)",
-                    'response' => []
-                ], 404);
-            }
-
-            Database::update("tb_racc", ['ACC_TOKEN' => $token], ['ID_ACC' => $account['ID_ACC']]);
-        }
-
-    default: break;
+$token = MetatraderFactory::autoConnect($account['ACC_LOGIN']);
+if(!$token) {
+    ApiResponse([
+        'status' => false,
+        'message' => "Invalid Connection",
+        'response' => []
+    ], 404);
 }
-
 
 ApiResponse([
     'status' => true,
