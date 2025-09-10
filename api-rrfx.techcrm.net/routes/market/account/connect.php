1dd1a84d090ecd5fb8d71d0ac4476a6e478eb8c1
Fani Annur Rozi <crazyhx4@gmail.com>
2025-09-10 10:52:53 +0700

    update api account connect
    otomatis menggunakan token yang sebelumnya (jka sudah pernah connect), dan jika tidak ada maka mengambil token baru dan disimpan untuk kedepannya

--- api-rrfx.techcrm.net/routes/market/account/connect.php
+++ api-rrfx.techcrm.net/routes/market/account/connect.php
@@ -3,6 +3,7 @@
 use App\Factory\MetatraderFactory;
 use App\Models\Account;
 use App\Models\Helper;
+use Config\Core\Database;
 
 $accountLogin = Helper::form_input($_POST['account'] ?? 0);
 $apiTerminal = MetatraderFactory::apiTerminal();
@@ -17,23 +18,58 @@ if(empty($account) || $account['ACC_MBR'] != $user['MBR_ID']) {
     ], 400);
 }
 
-/** Connect meta */
-$connectData = [
-    'login' => $account['ACC_LOGIN'], 
-    'password' => $account['ACC_PASS']
-];
+$isEmptyToken = empty($account['ACC_TOKEN']);
+$token = "";
+switch($isEmptyToken) {
+    case true:
+        /** Connect meta */
+        $connectData = [
+            'login' => $account['ACC_LOGIN'], 
+            'password' => $account['ACC_PASS']
+        ];
+        
+        $token = $apiTerminal->connect($connectData);
+        if(!$token) {
+            ApiResponse([
+                'status' => false,
+                'message' => "Invalid Connection",
+                'response' => []
+            ], 404);
+        }
 
-$token = $apiTerminal->connect($connectData);
-if(!$token) {
-    ApiResponse([
-        'status' => false,
-        'message' => "Invalid Connection",
-        'response' => []
-    ], 404);
+        Database::update("tb_racc", ['ACC_TOKEN' => $token], ['ID_ACC' => $account['ID_ACC']]);
+        break;
+
+    case false:
+        /** check connection with available token */
+        $account = $apiTerminal->accountSummary(['id' => $account['ACC_TOKEN']]);
+        if(!$account->success) {
+            /** get new token */
+            $connectData = [
+                'login' => $account['ACC_LOGIN'], 
+                'password' => $account['ACC_PASS']
+            ];
+            
+            $token = $apiTerminal->connect($connectData);
+            if(!$token) {
+                ApiResponse([
+                    'status' => false,
+                    'message' => "Invalid Connection (2)",
+                    'response' => []
+                ], 404);
+            }
+
+            Database::update("tb_racc", ['ACC_TOKEN' => $token], ['ID_ACC' => $account['ID_ACC']]);
+        }
+
+    default: break;
 }
+
 
 ApiResponse([
     'status' => true,
     'message' => "Successfull",
-    'response' => []
+    'response' => [
+        // 'token' => $token
+    ]
 ]);
