ca5ba282b90c65e18f858e58a1059d09f0b9d884
Fani Annur Rozi <crazyhx4@gmail.com>
2025-09-11 14:25:31 +0700

    fix: api market symbols

--- api-rrfx.techcrm.net/routes/market/symbols.php
+++ api-rrfx.techcrm.net/routes/market/symbols.php
@@ -1,25 +1,56 @@
 <?php
-global $ApiMeta;
 
-$symbols = $ApiMeta->symbols([]);
+use App\Factory\MetatraderFactory;
+use App\Models\Account;
+use App\Models\Helper;
+
+$apiTerminal = MetatraderFactory::apiTerminal();
+$accountLogin = Helper::form_input($_GET['account'] ?? 0);
+
+/** Check Account */
+$account = Account::realAccountDetail_byLogin($accountLogin);
+if(empty($account) || $account['ACC_MBR'] != $user['MBR_ID']) {
+    ApiResponse([
+        'status' => false,
+        'message' => "Invalid Account",
+        'response' => []
+    ], 400);
+}
+
+$token = MetatraderFactory::autoConnect($accountLogin);
+if(!$token) {
+    ApiResponse([
+        'status' => false,
+        'message' => "Invalid Connection (2)",
+        'response' => []
+    ], 400);
+}
+
+$symbols = $apiTerminal->symbols(['id' => $token]);
+if(!$symbols || !is_object($symbols)) {
+    ApiResponse([
+        'status' => false,
+        'message' => "Invalid Data",
+        'response' => []
+    ], 400);
+}
+
 if(!$symbols->success) {
     ApiResponse([
         'status' => false,
-        'message' => $symbols->error,
+        'message' => $symbols->message,
         'response' => []
     ], 400);
 }
 
 $result = [];
-foreach($symbols->message as $symbol) {
+foreach($symbols->data as $symbol) {
     $result[] = [
-        'symbol' => $symbol->symbol,
-        'contract_size' => $symbol->contractsize,
+        'symbol' => $symbol->currency,
+        'contract_size' => $symbol->contractSize,
         'spread' => $symbol->spread,
         'digits' => $symbol->digits,
-        'pricesettle' => $symbol->pricesettle,
         'trademode' => $symbol->trademode,
-        'margininitial' => $symbol->margininitial,
         'volume_min' => $symbol->volumeMin,
         'volume_max' => $symbol->volumeMax,
     ];
