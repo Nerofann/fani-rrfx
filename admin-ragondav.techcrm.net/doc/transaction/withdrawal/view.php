3a7118107f4a061f54e7ed4f6bb1cf1bf8b2d655
Muhammad Alfi Syahrin <malfisyahrin@allmediaindo.com>
2025-09-11 23:25:52 +0700

    fix breadcrumb home

--- admin-ragondav.techcrm.net/doc/transaction/withdrawal/view.php
+++ admin-ragondav.techcrm.net/doc/transaction/withdrawal/view.php
@@ -8,7 +8,7 @@
     <div>
         <h2 class="main-content-title tx-24 mg-b-5">Withdrawal</h2>
         <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="javascript:void(0);">Home</a></li>
+            <li class="breadcrumb-item"><a href="<?= pathbreadcrumb(0) ?>/dashboard">Home</a></li>
             <li class="breadcrumb-item"><a href="javascript:void(0);">Transaction</a></li>
             <li class="breadcrumb-item active" aria-current="page">Withdrawal</li>
         </ol>
