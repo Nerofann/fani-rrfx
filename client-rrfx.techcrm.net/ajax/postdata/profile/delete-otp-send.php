bd09e0a6c82fbdc8e1c07704d09d50688d8d6763
somdoode <somdoo777@gmail.com>
2025-09-12 13:38:28 +0700

    Update delete-otp-send.php

--- client-rrfx.techcrm.net/ajax/postdata/profile/delete-otp-send.php
+++ client-rrfx.techcrm.net/ajax/postdata/profile/delete-otp-send.php
@@ -4,32 +4,49 @@ use App\Models\Logger;
 use Config\Core\Database;
 use Config\Core\EmailSender;
 
-$otpCode = random_int(1000, 9999);
-$otpExpired = date("Y-m-d H:i:s", strtotime("+5 minute"));
-$update = Database::update("tb_member", 
-[
-    'MBR_OTP' => $otpCode,
-    'MBR_OTP_EXPIRED' => $otpExpired
-], [
-    'MBR_ID' => $user['MBR_ID']
-]);
-if(!$update) {
+try {
+    global $db;
+    mysqli_report(MYSQLI_REPORT_ERROR|MYSQLI_REPORT_STRICT);
+    mysqli_begin_transaction($db);
+
+    $otpCode = random_int(1000, 9999);
+    $otpExpired = date("Y-m-d H:i:s", strtotime("+5 minute"));
+    $update = Database::update("tb_member", 
+    [
+        'MBR_OTP' => $otpCode,
+        'MBR_OTP_EXPIRED' => $otpExpired
+    ], [
+        'MBR_ID' => $user['MBR_ID']
+    ]);
+    if(!$update) {
+        JsonResponse([
+            'success' => false,
+            'message' => "Gagal memperbarui data",
+            'data' => []
+        ]);
+    }
+
+    /** Email OTP for member*/
+    $emailData = [
+        'subject' => "Delete account OTP",
+        'otp'  => $otpCode,
+    ];
+
+    $emailSender = EmailSender::init(['email' => $user['MBR_EMAIL'], 'name' => $user['MBR_NAME']]);
+    $emailSender->useFile("otp-delete", $emailData);
+    $send = $emailSender->send();
+
+
+    mysqli_commit($db);
+} catch (Exception | mysqli_sql_exception $e) {
+    mysqli_rollback($db);
+
     JsonResponse([
         'success' => false,
-        'message' => "Gagal memperbarui data",
+        'message' => "Exception occured",
         'data' => []
     ]);
 }
-
-/** Email OTP */
-$emailData = [
-    'subject' => "Delete account OTP",
-    'otp'  => $otpCode,
-];
-
-$emailSender = EmailSender::init(['email' => $user['MBR_EMAIL'], 'name' => $user['MBR_NAME']]);
-$emailSender->useFile("otp-delete", $emailData);
-$send = $emailSender->send();
 
 /** Log */
 Logger::client_log([
