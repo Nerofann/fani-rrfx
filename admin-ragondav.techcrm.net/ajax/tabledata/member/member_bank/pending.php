003b3cd4181cf54fe3493b2a66d76843fb8994e5
Fani Annur Rozi <crazyhx4@gmail.com>
2025-09-12 19:44:15 +0700

    view pending member bank

---
+++ admin-ragondav.techcrm.net/ajax/tabledata/member/member_bank/pending.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Models\MemberBank;
+$dt->query("
+    SELECT
+        tmb.MBANK_DATETIME,
+        tm.MBR_NAME,
+        tm.MBR_EMAIL,
+        tmb.MBANK_NAME,
+        tmb.MBANK_HOLDER,
+        tmb.MBANK_ACCOUNT,
+        tmb.ID_MBANK
+    FROM tb_member_bank tmb
+    JOIN tb_member tm ON (tm.MBR_ID = tmb.MBANK_MBR)
+    WHERE MBANK_STS = ".MemberBank::$statusPending."
+");
+
+$dt->hide("MBR_NAME");
+$dt->edit("MBR_EMAIL", function($col) {
+    return '
+        <p class="mb-0">'.$col['MBR_NAME'].'</p>
+        <p class="mb-0">'.$col['MBR_EMAIL'].'</p>
+    ';
+});
+
+$dt->hide("MBANK_NAME");
+$dt->hide("MBANK_HOLDER");
+$dt->edit("MBANK_ACCOUNT", function($col) {
+    return '
+        <p class="mb-0">'.$col['MBANK_NAME'].'</p>
+        <p class="mb-0">'.$col['MBANK_HOLDER'].' / '.$col['MBANK_ACCOUNT'].'</p>
+    ';
+});
+
+$dt->edit("ID_MBANK", function($col) {
+    return '
+        <div class="action d-flex gap-2 justify-content-center align-items-center" data-id="'.md5(md5($col['ID_MBANK'])).'">
+           
+        </div>
+    ';
+});
+
+echo $dt->generate()->toJson();
